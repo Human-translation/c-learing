cd2dff29ae3387e0ab9b1a24d2100ff45cf6260c
976898643@qq.com <976898643@qq.com>
2020-03-21 23:12:41 +0800

    练习

---
+++ 20_3_21/20_3_21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_set>
+#include <memory.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;   
+/*
+    int removeDuplicates(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto it = nums.begin();it!=nums.end();it++)
+        {
+            
+        }
+    }
+*/
+    int main()
+    {
+        unordered_set<int> s;
+        vector<int> num={1,1,2};
+        s.count(1);
+        system("pause");
+        return 0;
+    }
