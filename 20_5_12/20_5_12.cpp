f111777f319cc2e528c5774978e1de619b124fa2
976898643@qq.com <976898643@qq.com>
2020-05-12 22:09:32 +0800

    linux

---
+++ 20_5_12/20_5_12.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+//需要进行读操作
+
+int main()
+{
+    int fd = open("./a.txt", O_RDWR);
+    if(fd < 0)
+    {
+        perror("opne");
+        return 0;
+    }
+
+    char buf[1024] = {0};
+    //要注意在数组当中预留\0的位置
+    while(1)
+    {
+        memset(buf, '\0', 1024);
+        read(fd, buf, sizeof(buf) - 1);
+        printf("i am read [%s]\n", buf);
+    }
+
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
